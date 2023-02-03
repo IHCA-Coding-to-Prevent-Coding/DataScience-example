bc1c56ec2a937f5042f7bdc6698599fe95c072b9
atindewan <111815018+atindewan@users.noreply.github.com>
2023-02-02 19:04:13 -0500

    Add files via upload

---
+++ first_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  2 18:50:57 2023
+
+@author: atindewan
+"""
+
+import pandas as pd
+#import numpy as np
+
+dia = pd.read_csv("/Users/atindewan/Desktop/Atin/Purdue/NUR 39900/mimic-iv-ed-2.2/ed/diagnosis.csv")
+
+print(dia.head())
